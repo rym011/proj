b4ab53b61f9e5ce59792d23fe08f81ea9dd27318
rym011 <rim.ghnania@isimg.tn>
2024-12-18 14:33:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-function valide(){
+function valide() {
     document.getElementById('quiz-form').addEventListener('submit', function(event) {
         event.preventDefault();
         let score = 0;
-        let answers = { q1: 'Nettoyage', q2: 'Peau grasse' }; // Réponses correctes
+
+        let answers = { 
+            q1: 'Nettoyage', 
+            q2: 'Peau grasse', 
+            q3: 'Deux fois', 
+            q4: 'Aloe vera',
+            q5: 'Après la crème hydratante',
+            q6: 'Une à deux fois par semaine',
+            q7: 'Acide salicylique'
+        };
+
         for (let question in answers) {
             let userAnswer = document.querySelector(`input[name="${question}"]:checked`);
             if (userAnswer && userAnswer.value === answers[question]) {
                 score++;
             }
         }
-        document.getElementById('result').textContent = alert(`Vous avez ${score} bonnes réponses !`);;
+        alert(`Vous avez ${score} bonnes réponses !`);
     });
 }
+
 function envoi() {
     document.getElementById("contactForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Empêche l'envoi par défaut du formulaire
+        event.preventDefault(); 
 
         const name = document.getElementById("name").value.trim();
         const email = document.getElementById("email").value.trim();
         const message = document.getElementById("message").value.trim();
-        // Vérification
+        
         if (name === "" || email === "" || message === "") {
             alert("Veuillez remplir tous les champs avant de cliquer sur Envoyer !");
         } else {
             alert("Votre message a été envoyé avec succès ! Merci de m'avoir contacté.");
 
-            document.getElementById("contactForm").reset(); // Réinitialise le formulaire
+            document.getElementById("contactForm").reset(); 
         }
     });
 }
-
